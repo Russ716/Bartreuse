0fd631d8f185cb9cefe0bc504e9cc0fcd8e24329
Russ Pelton <iamkrashenbern@yahoo.com>
2022-09-12 14:32:38 -0500

    Algorithm planning and sales data in json.

---
+++ algorithm.js
@@ -0,0 +1,29 @@
+// ! React Homepage
+// ? Login Code block
+// * Register new user button
+
+// ! main page
+// * New Inventory Audit
+
+// ! New Inventory Audit
+// * New Bottle
+// * Manage Data
+// * New Audit Count
+
+// ! New Bottle
+// ? Transient State:
+// * Text input fields
+// * Checkbox for openable
+// * Save Button sends to database
+
+// ! Manage Data
+// * Current Bottle List, lists all current bottles in bar
+// * Previous audits from Database, listed
+
+// ! New Audit Count
+// ? Transient State:
+// * text input fields
+// * Dropdown for Container
+// * Save button sends to database
+// * List shows current audit from database
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import { Authorized } from "./views/Authorized"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import "./bartreuse.css"
+
+
+export const Bartreuse = () => {
+  return <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+
+    <Route path="*" element={
+      <Authorized>
+        <>
+          <NavBar />
+          
+        </>
+      </Authorized>
+
+    } />
+  </Routes>
 }
 
-export default App;
